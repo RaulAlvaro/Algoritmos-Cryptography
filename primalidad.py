da97f5c27d470ebeac0f45dc8b72146d170f8a8b
Raul <raulalvaro123@hotmail.com>
2019-10-16 22:44:49 -0500

    fix primalidad for big input

--- primalidad.py
+++ primalidad.py
@@ -12,40 +12,21 @@ def potenciacion(x,b,n):
 			z = z * x % n
 	return z
 
-
 n = input("Ingresa el número: ")
 
 if (n[-1] != '1') and (n[-1] != '3') and (n[-1] != '7') and (n[-1] != '9'):
 	print("Es compuesto")
 else:
 	n = int(n)
-	#k = 1
-	#m = 1
-	#ispar = False
-	#35797
-	#Obtencion de k y m es ineficiente
-	"""
-	while (2**k)*m < n-1 or (ispar==False):
-		print('El valor de k es',k)
-		while (2**k)*m < n-1:
-			m = m+1
-			print('El valor de m es',m)
-			print('2**k*m es', (2**k*m))
-		if (2**k)*m == n-1 and (m%2 != 0):
-			ispar = True
-		else:
-			k = k+1
-			m = 1
-	"""
 	ntemp = n-1
 	k = 0
 	m = 0
 	while ntemp % 2 == 0:
 		if ntemp%2 == 0:
 			k = k + 1
-			print("El valor de k es: ", k)
+			#print("El valor de k es: ", k)
 			ntemp = ntemp/2
-			print("El valor de ntemp es: ", ntemp)
+			#print("El valor de ntemp es: ", ntemp)
 	m = int(ntemp)
 	k = int(k)
 	print('El valor de k es',k)
